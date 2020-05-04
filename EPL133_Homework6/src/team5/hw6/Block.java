7898ead6c66d349afaa6ded5d5926bf8f55ed368
celeft01 <celeft01@ucy.ac.cy>
2020-05-04 23:11:36 +0300

    added block class

---
+++ EPL133_Homework6/src/team5/hw6/Block.java
@@ -0,0 +1,20 @@
+package team5.hw6;
+
+public class Block {
+
+	private int x;
+	private int y;
+	private boolean border;
+	private Grid g;
+	
+	public Block(int n1, int n2, boolean b, Grid grid) {
+		x=n1;
+		y=n2;
+		border=b;
+		g=grid;
+	}
+	
+	
+	
+	
+}
